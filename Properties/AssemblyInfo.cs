445e84a444995af174b78071091cbf838c33e537
Desktop-PC <Desktop-PC@ELITE>
2022-08-01 17:49:54 -0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using TestModForChilloutVR;
+// ...
+[assembly: MelonInfo(typeof(TestModClassForChilloutVR), "TestModForChilloutVR", "1.0.0", "Eliteiment")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestModForChilloutVR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestModForChilloutVR")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33afca0c-f220-4a3c-841c-4f2d8d3114ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestModClassForChilloutVR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+
+namespace TestModForChilloutVR
+{
+    public class TestModClassForChilloutVR : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                ABI_RC.Systems.MovementSystem.MovementSystem.Instance.ToggleFlight();
+            }
+        }
+    }
+}
